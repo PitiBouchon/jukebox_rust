f272c0ff3cce847e2a6c1d83d33c6760a2769abe
PtiBouchon <lopite2@live.fr>
2023-02-09 00:09:14 +0100

    Add libmpv to Cargo.toml

--- entity/src/user.rs
+++ entity/src/user.rs
@@ -1,7 +1,7 @@
 use sea_orm::entity::prelude::*;
 use serde::Deserialize;
 
-#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Deserialize)]
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Deserialize)] // TODO : remove the Debug
 #[sea_orm(table_name = "users")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
